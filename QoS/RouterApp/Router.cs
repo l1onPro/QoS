b05bf8fa6e6e8f17e6a4ac0ac2880c754d869732
Ilya <ilya_barbarich@mail.ru>
2019-05-08 08:28:18 +0300

    Change structure project

---
+++ QoS/RouterApp/Router.cs
@@ -0,0 +1,19 @@
+﻿using QoS.Algorithms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QoS.RouterApp
+{
+    class Router
+    {
+        IAlgorithm algorithm;
+        
+        public Router ()
+        {
+
+        }
+    }
+}
