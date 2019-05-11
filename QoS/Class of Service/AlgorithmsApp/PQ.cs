11b1c6af8807cb770a76234c286010ba1a6863aa
Ilya <ilya_barbarich@mail.ru>
2019-05-11 09:12:55 +0300

    Add PQ algorithm

---
+++ QoS/Class of Service/AlgorithmsApp/PQ.cs
@@ -0,0 +1,22 @@
+﻿using QoS.AppPackage;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QoS.Class_of_Service.AlgorithmsApp
+{
+    class PQ : IAlgorithm
+    {
+        private List<Queuering> listQueue;
+        public PQ(List<Package> allPackage)
+        {
+
+        }
+        public List<Queuering> getAllQueues()
+        {
+            return listQueue;
+        }
+    }
+}
